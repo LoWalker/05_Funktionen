d681d4921aaa403b47128f2592996beb32d835f0
Lawrence <lawrence.langen@gmail.com>
2023-01-17 12:49:01 +0100

    Funktion_03a

--- script_01.js
+++ script_01.js
@@ -48,11 +48,31 @@
 /*******Funktionen 02c *****/
 // 2c Mehrere Parameter
 
-ausgabeNamenParams("Max", "Mütze");
-ausgabeNamenParams(prompt("Vorname?"),prompt("Nachname?"));
+// ausgabeNamenParams("Max", "Mütze");
+// ausgabeNamenParams(prompt("Vorname?"),prompt("Nachname?"));
 
+
+// function ausgabeNamenParams(firstName, familyName) {
+//     console.log("hallo " + firstName + " " + familyName +"!");
+// }
+
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+ausgabeNamenParams("Max", "Mütze");
 
 function ausgabeNamenParams(firstName, familyName) {
-    console.log("hallo " + firstName + " " + familyName +"!");
+
+    // 1. Funktionalität: string composing
+    const gap = " ";
+    let outputStr = "Hallo" + gap + firstName + gap + familyName +"!";
+
+
+    // 2. Funktionalität: string output
+
+    console.log(outputStr);
 }
 
