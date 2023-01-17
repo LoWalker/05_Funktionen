74368eaec0b72500f29fd395dc2663732a5908a4
Lawrence <lawrence.langen@gmail.com>
2023-01-17 11:56:46 +0100

    Funktionen_02b

--- script_01.js
+++ script_01.js
@@ -17,13 +17,30 @@
 // 2a, Parametrisierung + Datenübergabe von INNEN
 
 
-ausgabeNamen();
-// console.log(firstName); // Fehler; Scope!
+// // ausgabeNamen();
+// // // console.log(firstName); // Fehler; Scope!
 
-function ausgabeNamen() {
-    let firstName = "Martin"; // what happens in Vegas, ...
-    console.log("Hallo " + firstName + "!");
+// // function ausgabeNamen() {
+// //     let firstName = "Martin"; // what happens in Vegas, ...
+// //     console.log("Hallo " + firstName + "!");
+// }
+
+/*****Funktionen 02b ******/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+// Mein Beispiel
+// function ausgabeNamenParam(firstName) {
+//     const begruessung = "Hallo " + firstName + "!";
+//     return begruessung;
+// }
+// console.log(ausgabeNamenParam("Herr Schmidt"));
+
+// Beispiel von Dozent
+
+ausgabeNamenParam("Martin"); // Call + Argument(e)
+ausgabeNamenParam("Lawrence");
+// Argumente sind Daten für Parameter
+
+function ausgabeNamenParam(firstName) { //Funktion + Parameter
+    console.log("hallo " + firstName + "!");
 }
-
-
-
