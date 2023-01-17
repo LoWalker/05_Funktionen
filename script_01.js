242c619d07744390bd059ae132f8c5a1ef9adc4f
Lawrence <lawrence.langen@gmail.com>
2023-01-17 14:50:23 +0100

    Letzte_Commit

--- script_01.js
+++ script_01.js
@@ -6,94 +6,80 @@
 // Funktionsaufruf (call)
 // test();
 
-// // Funktionsrumpf (body) | callee
-// // Funktionsdeklaration
-// function test()
-// {
-//     console.log("Hallo Patrick!");
-// }
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration
+function test()
+{
+    console.log("Hallo Patrick!");
+}
 
-/********Funktionen 02a *****/
-// 2a, Parametrisierung + Datenübergabe von INNEN
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
 
+// ausgabeNamen();
+// console.log(firstName); // Fehler: Scope!
 
-// // ausgabeNamen();
-// // // console.log(firstName); // Fehler; Scope!
+function ausgabeNamen() {
+    let firstName = "Heinrich"; // what happens in Vegas, ...
+    console.log("Hallo " + firstName + "!");
+}
 
-// // function ausgabeNamen() {
-// //     let firstName = "Martin"; // what happens in Vegas, ...
-// //     console.log("Hallo " + firstName + "!");
-// }
-
-/*****Funktionen 02b ******/
+/***** Funktionen 02b *****/
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
-
-// Mein Beispiel
-// function ausgabeNamenParam(firstName) {
-//     const begruessung = "Hallo " + firstName + "!";
-//     return begruessung;
-// }
-// console.log(ausgabeNamenParam("Herr Schmidt"));
-
-// Beispiel von Dozent
 
 // ausgabeNamenParam("Martin"); // Call + Argument(e)
 // ausgabeNamenParam("Lawrence");
-// // Argumente sind Daten für Parameter
+// Argumente sind Daten für Parameter
 
-// function ausgabeNamenParam(firstName) { //Funktion + Parameter
-//     console.log("hallo " + firstName + "!");
-// }
+function ausgabeNamenParam(firstName) { //Funktion + Parameter
+    console.log("Hallo " + firstName + "!");
+}
 
-/*******Funktionen 02c *****/
-// 2c Mehrere Parameter
+/***** Funktionen 02c *****/
+// 2c. Mehrere Parameter
 
-// ausgabeNamenParams("Max", "Mütze");
+// ausgabeNamenParams("Max","Mütze");
 // ausgabeNamenParams(prompt("Vorname?"),prompt("Nachname?"));
 
-
-// function ausgabeNamenParams(firstName, familyName) {
-//     console.log("hallo " + firstName + " " + familyName +"!");
-// }
-
+function ausgabeNamenParams(firstName, familyName) {
+    console.log("Hallo " + firstName + " " + familyName +"!");
+}
 
 /***** Funktionen 03a *****/
 // 03a. Vorbereitung
 // Postulat: one function = one job (uncle Bob)
 // SRP single responsibility principle
 
-// ausgabeNamenParams("Max", "Mütze");
+// ausgabeNamenParams2("Max","Mütze");
 
-function ausgabeNamenParams(firstName, familyName) 
-{
+function ausgabeNamenParams2(firstName, familyName) {
+
+    // 1. Funktionalität: string composing
     const gap = " ";
     let outputStr = "Hallo" + gap + firstName + gap + familyName +"!";
+
+    // 2. Funktionalität: string output
+    console.log(outputStr);
 }
-//     // 1. Funktionalität: string composing
-    
 
-
-//     // 2. Funktionalität: string output
-
-
-/******Funktionen 03b ****/
+/***** Funktionen 03b *****/
 // Trennen der Funktionalitäten | return
 
 
-// 1. Funktionalität: sting composing
-output(getString("Max","Mütze")); //call
+// 1. Funktionalität: string composing
+output(getString("Max","Mütze")); // call
 output("Hallo Max Mütze!");
-function getString(firstName, familyName) 
-{
+function getString(firstName, familyName) {
     const GAP = " ";
     let outputStr = "Hallo" + GAP + firstName + GAP + familyName +"!";
-    return outputStr // return sendet Daten an den call zurück
+    return outputStr; // return sendet Daten an den call zurück
     console.log("Funktion beendet"); // return bricht die Fkt ab!
 }
 
-
 /* 2. Funktionalität: string output */
-// output("Hi");// Call ...
+// output("Hi"); // Call ...
+// output(2);
+// output(true);
 function output(outputData) {
-    console.log(outputData)
+    console.log(outputData); 
 }
